0b5f5869f13d12a564d6f426b51d0edd4cd4d262
Samuli Juppi <Samuli.Juppi@metropolia.fi>
2019-08-21 17:03:43 +0300

    final tweaks for space comp. ---2h

--- client/src/components/Space.jsx
+++ client/src/components/Space.jsx
@@ -2,21 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useQuery } from 'react-apollo-hooks';
-import {
-  Accordion,
-  Card,
-  Button,
-  Container,
-  Row,
-  Col,
-  Spinner,
-  Image,
-} from 'react-bootstrap';
+import { Accordion, Card, Container, Row, Col, Spinner } from 'react-bootstrap';
 import { GET_LAUNCH_DATA } from '../services/queries';
 import rocket from '../util/img/rocket.png';
 
 const Space = ({ me }) => {
   const [t, setT] = useState({});
+  const [filter, setFilter] = useState({
+    rocket: 'all',
+    order: 'desc',
+    scope: 'all',
+  });
   const [intervalId, setIntervalId] = useState(null);
   const {
     data: { getLaunchData },
@@ -27,7 +23,16 @@ const Space = ({ me }) => {
     return () => clearInterval(intervalId);
   }, [intervalId]);
 
+  useEffect(() => window.scrollTo(0, 0), []);
+
   const getTime = (launchDate, missionName) => {
+    /*if (launchDate - new Date().getTime() / 1000 < 0) {
+      setT({
+        time: 'Expired',
+        mission: missionName,
+      });
+      return null;
+    }*/
     console.log('stuff', { date: launchDate, name: missionName });
     if (missionName === t.mission) {
       console.log('here');
@@ -46,17 +51,49 @@ const Space = ({ me }) => {
         const minutes = Math.floor((distance % (60 * 60)) / 60);
         const seconds = Math.floor(distance % 60);
 
-        setT({
-          time: `${days}d : ${hours}h : ${minutes}m : ${seconds}s`,
-          mission: missionName,
-        });
+        if (distance > 0) {
+          setT({
+            time: `${days}d : ${hours}h : ${minutes}m : ${seconds}s`,
+            mission: missionName,
+          });
+        } else {
+          setT({
+            time: 'Expired',
+            mission: missionName,
+          });
+        }
       }, 1000);
 
       setIntervalId(interval);
     }
+    return null;
+  };
+
+  const filterMissions = () => {
+    let missions = [];
+
+    if (filter.scope === 'all') {
+      missions = JSON.parse(getLaunchData.value);
+    } else {
+      missions = JSON.parse(getLaunchData.value).filter(msn => msn.upcoming);
+    }
+
+    if (filter.rocket !== 'all') {
+      missions = missions.filter(
+        msn => msn.rocket.rocket_name === filter.rocket
+      );
+    }
+
+    if (filter.order === 'desc') {
+      return missions.sort((a, b) => a.launch_date_unix - b.launch_date_unix);
+    }
+
+    return missions.sort((a, b) => b.launch_date_unix - a.launch_date_unix);
   };
 
   if (!me) return null;
+
+  if (!loading) console.log(JSON.parse(getLaunchData.value));
 
   if (loading) {
     return (
@@ -78,81 +115,130 @@ const Space = ({ me }) => {
   }
 
   return (
-    <div className="container">
-      <h2 className="pb-3" style={{ textAlign: 'center' }}>
-        Upcoming Space X Missions
+    <div className="container pt-4 mt-4">
+      <h2 className="pb-3 mb-3" style={{ textAlign: 'center' }}>
+        Space X Missions
       </h2>
+      <Container className="mb-4">
+        <Row>
+          <Col xs={12} sm={12} md={4}>
+            Rocket:
+            <select
+              style={{ width: '100%' }}
+              onChange={event =>
+                setFilter({ ...filter, rocket: event.target.value })
+              }
+            >
+              <option value="all">All</option>
+              {!loading &&
+                JSON.parse(getLaunchData.value)
+                  .map(mis => mis.rocket.rocket_name)
+                  .filter((v, i, a) => a.indexOf(v) === i)
+                  .map(rname => (
+                    <option key={rname} value={rname}>
+                      {rname}
+                    </option>
+                  ))}
+            </select>
+          </Col>
+          <Col xs={12} md={4} sm={12}>
+            Order:
+            <select
+              style={{ width: '100%' }}
+              onChange={event =>
+                setFilter({ ...filter, order: event.target.value })
+              }
+            >
+              <option value="desc">Descending</option>
+              <option value="asc">Ascending</option>
+            </select>
+          </Col>
+          <Col xs={12} sm={12} md={4}>
+            Scope:
+            <select
+              style={{ width: '100%' }}
+              onChange={event =>
+                setFilter({ ...filter, scope: event.target.value })
+              }
+            >
+              <option value="all">All</option>
+              <option value="upcoming">Upcoming</option>
+            </select>
+          </Col>
+        </Row>
+      </Container>
       <Accordion>
         {!loading &&
-          JSON.parse(getLaunchData.value)
-            .filter(flight => flight.upcoming)
-            .sort((a, b) => a.launch_date_unix - b.launch_date_unix)
-            .map((launch, i) => (
-              <Card key={`${launch.mission_name}`}>
-                <Card.Header>
-                  <Accordion.Toggle
-                    as={Button}
-                    variant="link"
-                    eventKey={`${i}`}
-                    onClick={() =>
-                      getTime(launch.launch_date_unix, launch.mission_name)
-                    }
-                  >
-                    {launch.mission_name}
-                  </Accordion.Toggle>
-                  <p>Launching: {launch.launch_date_utc.substring(0, 10)}</p>
-                </Card.Header>
-                <Accordion.Collapse eventKey={`${i}`}>
-                  <Card.Body>
-                    <h4 style={{ textAlign: 'center' }}>Mission details</h4>
-                    <Container className="pt-3">
-                      <Row>
-                        <Col
-                          sm={{ span: 12, order: 2 }}
-                          md={{ order: 1, span: 8 }}
-                          xs={{ span: 12, order: 2 }}
-                        >
-                          <dl>
-                            <dt>Description</dt>
-                            {launch.details ? (
-                              <dd>{launch.details}</dd>
-                            ) : (
-                              <dd>TBA</dd>
-                            )}
-                            <dt>Rocket</dt>
-                            {launch.rocket.rocket_id ? (
-                              <dd>{launch.rocket.rocket_name}</dd>
-                            ) : (
-                              <dd>TBA</dd>
-                            )}
-                            <dt>Launch site</dt>
-                            {launch.launch_site.site_id ? (
-                              <dd>{launch.launch_site.site_name_long}</dd>
-                            ) : (
-                              <dd>TBA</dd>
-                            )}
-                            <dt>Countdown</dt>
-                            <dd>{t.time}</dd>
-                          </dl>
-                        </Col>
-                        <Col
-                          xs={{ order: 1, span: 12 }}
-                          sm={{ order: 1, span: 12 }}
-                          md={{ order: 2 }}
-                          className="pb-3 text-center"
-                        >
-                          <img
-                            src={rocket}
-                            alt="rocket"
-                            style={{ height: '10rem' }}
-                          />
-                        </Col>
-                      </Row>
-                    </Container>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            ))}
+          filterMissions().map((launch, i) => (
+            <Card key={`${launch.mission_name}`}>
+              <Accordion.Toggle
+                as={Card.Header}
+                eventKey={`${i}`}
+                onClick={() =>
+                  getTime(launch.launch_date_unix, launch.mission_name)
+                }
+              >
+                {launch.mission_name}{' '}
+                <div style={{ float: 'right' }}>
+                  {launch.launch_date_utc.substring(0, 10)}
+                </div>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey={`${i}`}>
+                <Card.Body>
+                  <h4 style={{ textAlign: 'center' }}>Mission details</h4>
+                  <Container className="pt-3">
+                    <Row>
+                      <Col
+                        sm={{ span: 12, order: 2 }}
+                        md={{ order: 1, span: 8 }}
+                        xs={{ span: 12, order: 2 }}
+                      >
+                        <dl>
+                          <dt>Description</dt>
+                          {launch.details ? (
+                            <dd>{launch.details}</dd>
+                          ) : (
+                            <dd>TBA</dd>
+                          )}
+                          <dt>Rocket</dt>
+                          {launch.rocket.rocket_id ? (
+                            <dd>{launch.rocket.rocket_name}</dd>
+                          ) : (
+                            <dd>TBA</dd>
+                          )}
+                          <dt>Launch site</dt>
+                          {launch.launch_site.site_id ? (
+                            <dd>{launch.launch_site.site_name_long}</dd>
+                          ) : (
+                            <dd>TBA</dd>
+                          )}
+                          <dt>Countdown</dt>
+                          <dd>{t.time}</dd>
+                        </dl>
+                      </Col>
+                      <Col
+                        xs={{ order: 1, span: 12 }}
+                        sm={{ order: 1, span: 12 }}
+                        md={{ order: 2 }}
+                        className="pb-3 text-center"
+                      >
+                        <img
+                          src={rocket}
+                          alt="rocket"
+                          style={{ height: '10rem' }}
+                        />
+                      </Col>
+                    </Row>
+                  </Container>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          ))}
+        {!loading && filterMissions().length === 0 && (
+          <div className="text-center mt-5 pt-5">
+            <strong>No matches</strong>
+          </div>
+        )}
       </Accordion>
     </div>
   );
