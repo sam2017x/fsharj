34b1444b6d78d43724a6e67c91f1cc5a25e7eb20
sam2017x <43538975+sam2017x@users.noreply.github.com>
2019-09-30 20:55:47 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,8 +23,7 @@ server.installSubscriptionHandlers(httpServer);
 mongoose.set("useFindAndModify", false);
 
 // Use local or cloud DB.
-// const MONGODB_URI = `mongodb+srv://fs:fs@cluster0-tjvic.mongodb.net/AppMashup?retryWrites=true`;
-const MONGODB_URI = `mongodb://localhost:27017/ChatApp?retryWrites=true`;
+const MONGODB_URI = `mongodb+srv://fs:fs@cluster0-tjvic.mongodb.net/AppMashup?retryWrites=true`;
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
